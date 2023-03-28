1527502fff05aa479c5ac6963e78f1e6980b00d8
peteral2 <winstudium@gmail.com>
2023-03-28 10:46:10 +0200

    Added class MathLib and tests MathLibTests

---
+++ src/test/java/com/example/demo/demo/MathLibTests.java
@@ -0,0 +1,14 @@
+package com.example.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MathLibTests {
+
+    @Test
+    public void testEven1() {
+        assertTrue(MathLib.isEven(2));
+    }
+    
+}
