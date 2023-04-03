0afe2b5d6dcbceab1c3a74c3a1dd4804124eceef
peteral2 <winstudium@gmail.com>
2023-04-03 12:32:02 +0200

    Test 02 added

--- src/test/java/com/example/demo/demo/StringCalculatorTests.java
+++ src/test/java/com/example/demo/demo/StringCalculatorTests.java
@@ -6,9 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringCalculatorTests {
 
     @Test
-    public void OneInteger() {
+    public void OneInteger1() {
         StringCalculator calculator = new StringCalculator();
-        assertEquals(5,calculator.add("5"));
+        assertEquals(5, calculator.add("5"));
+
+    }
+    
+    @Test
+    public void OneInteger2() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(7, calculator.add("7"));
 
     }
     
