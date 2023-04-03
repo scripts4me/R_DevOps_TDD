5b8beb4b45bfb0e34c40669f69b096e8dea5f89b
peteral2 <winstudium@gmail.com>
2023-04-03 13:08:46 +0200

    Logik 04 added

--- src/main/java/com/example/demo/demo/StringCalculator.java
+++ src/main/java/com/example/demo/demo/StringCalculator.java
@@ -6,8 +6,23 @@ public class StringCalculator {
         if (numbers == "") {
             return 0;
         } else {
-            return Integer.valueOf(numbers);
+            String[] submittedNumbers = numbers.split(",");
+            int finalResult = calculateSum(submittedNumbers);
+            return finalResult;
         }
+    }
+
+    private int calculateSum(String[] inputSplittedByDelimiter) {
+        int result = 0;
+        for (String token : inputSplittedByDelimiter) {
+            result += addSingleToken(token);
+        }
+        return result;
+    }
+
+    private int addSingleToken(String token) {
+        Integer valueAsInteger = Integer.parseInt(token);
+        return valueAsInteger;
     }
 
 }
